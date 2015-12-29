bf3b70321ce710bfd734960bf56f02e778f7af51
jonhcw <jonhcw@gmail.com>
2015-12-29 16:30:40 +0200

    Created CreateAzureVM.ps1 with initial parameters

---
+++ CreateAzureVM/CreateAzureVM/CreateAzureVM.ps1
@@ -0,0 +1,25 @@
+﻿#
+# CreateAzureVM.ps1
+#
+
+[CmdletBinding()]
+Param(
+	[Parameter(Mandatory=$True)]
+	[string]$VMName,
+
+	[Parameter(Mandatory=$True)]
+	[string]$VMSize,
+
+	[Parameter(Mandatory=$True)]
+	[String]$Password,
+
+	[Parameter(Mandatory=$True)]
+	[string]$AdminUsername,
+
+	[Parameter(Mandatory=$True)]
+	[string]$AffinityGroup,
+
+	[Parameter(Mandatory=$True)]
+	[string]$ServiceName
+)
+
