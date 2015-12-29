b66bcd0ea961771c14279666548770a637431deb
jonhcw <jonhcw@gmail.com>
2015-12-29 16:45:05 +0200

    Some parametevalidation added

--- CreateAzureVM/CreateAzureVM/CreateAzureVM.ps1
+++ CreateAzureVM/CreateAzureVM/CreateAzureVM.ps1
@@ -8,6 +8,7 @@ Param(
 	[string]$VMName,
 
 	[Parameter(Mandatory=$True)]
+	[ValidateSet("ExtraSmall", "Small", "Medium", "Large", "ExtraLarge")]
 	[string]$VMSize,
 
 	[Parameter(Mandatory=$True)]
@@ -17,9 +18,11 @@ Param(
 	[string]$AdminUsername,
 
 	[Parameter(Mandatory=$True)]
+	[ValidateScript({Get-AzureAffinityGroup -Name $_})]
 	[string]$AffinityGroup,
 
 	[Parameter(Mandatory=$True)]
+	[ValidateScript({Get-AzureService -ServiceName $_})]
 	[string]$ServiceName
 )
 
