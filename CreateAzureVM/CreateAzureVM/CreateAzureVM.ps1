00f2097ea7ef756823221886fefe32f177ba92e7
jonhcw <jonhcw@gmail.com>
2015-12-29 19:29:37 +0200

    Comments, typo fixes and some general cleanup

--- CreateAzureVM/CreateAzureVM/CreateAzureVM.ps1
+++ CreateAzureVM/CreateAzureVM/CreateAzureVM.ps1
@@ -26,6 +26,8 @@ Param(
 	[string]$ServiceName
 )
 
+
+# Run commands to actually create the VM. Run as a Job so we can print status information
 $job = Start-Job -ScriptBlock {
 	param ($vname, $vsize, $pw, $user, $group, $svc)
 	$ImageName = (get-azurevmimage | ? {$_.imagename -like "*2012*datacenter*"} |  Sort-Object -Descending publisheddate)[0].imagename
@@ -34,20 +36,22 @@ $job = Start-Job -ScriptBlock {
 	$ProvisioningConfig | New-AzureVM -AffinityGroup $group -ServiceName $svc
 	} -ArgumentList $VMName, $VMSize, $Password, $AdminUsername, $AffinityGroup, $ServiceName
 
+
+# Print status information for New-AzureVM command
 Write-Host "Running New-AzureVM" -NoNewline
-while (!([string]($job | get-job).State).Equals("Completed")) {
+while (!([string]($job | get-job).State).Equals("Completed") -or !([string]($job | get-job).State).Equals("Failed")) {
 	Write-Host "." -NoNewline
 	Start-Sleep -Seconds 1
 }
 
 Write-Host "`nNew-AzureVM command completed!"
 
+# Wait for the VM status to become "ReadyRole". If there are any error codes, break and report.
 $VM = Get-AzureVM -ServiceName $ServiceName -Name $VMName
-
 $VMLastStatus = ""
 while (!$VM.Status.Equals("ReadyRole")) {
-	if ($VMName.InstanceErrorCode -ne $null) {
-		Write-Error $VMName.InstanceErrorCode
+	if ($VM.InstanceErrorCode -ne $null) {
+		Write-Error $VM.InstanceErrorCode
 		break
 	}
 
@@ -55,13 +59,18 @@ while (!$VM.Status.Equals("ReadyRole")) {
 	if ($VMLastStatus -eq $VM.Status) { 
 		Write-Host "." -NoNewline
 	} else {
-		Write-Host "`n$($VM.Status)" -NoNewline
+		Write-Host "`nVM in status $($VM.Status)" -NoNewline
 	}
 	$VMLastStatus = $VM.Status
 	Start-Sleep -Seconds 1
 }
 
-if ($VM.Status.Equals("ReadyRole")) {
+# Not sure what exactly are the indicators that it deployed totally succesfully. I'm assuming
+
+if ($VM.InstanceErrorCode -ne $null) {
+	Write-Host "`nVM deployed with errors"
+}
+elseif ($VM.Status.Equals("ReadyRole")) {
 	Write-Host "`nVM deployed succesfully"
 } else {
 	Write-Host "`nVM deployed with errors"
