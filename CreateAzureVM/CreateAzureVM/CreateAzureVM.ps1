72256d608e5ef1764e9535cc68f8529f97d07942
jonhcw <jonhcw@gmail.com>
2015-12-29 18:13:36 +0200

    Added status tracking. will need to create job if I want to report on running "new-azurevm"

--- CreateAzureVM/CreateAzureVM/CreateAzureVM.ps1
+++ CreateAzureVM/CreateAzureVM/CreateAzureVM.ps1
@@ -27,4 +27,31 @@ Param(
 )
 
 $VMConfig = New-AzureVMConfig -Name $VMName -InstanceSize $VMSize -ImageName a699494373c04fc0bc8f2bb1389d6106__Windows-Server-2012-Datacenter-20151120-en.us-127GB.vhd
-$VMConfig | Add-AzureProvisioningConfig -Windows -Password $Password -AdminUsername $AdminUsername | New-AzureVM -AffinityGroup $AffinityGroup -ServiceName $ServiceName
+$ProvisioningConfig = $VMConfig | Add-AzureProvisioningConfig -Windows -Password $Password -AdminUsername $AdminUsername
+
+Write-Host "Running New-AzureVM command"
+$VM = $ProvisioningConfig | New-AzureVM -AffinityGroup $AffinityGroup -ServiceName $ServiceName
+Write-Host "Running New-AzureVM command completed!"
+
+$VMLastStatus = ""
+while (!$VM.Status.Equals("ReadyRole")) {
+	if ($VMName.InstanceErrorCode -ne $null) {
+		Write-Error $VMName.InstanceErrorCode
+		break
+	}
+
+	$VM = $VM | Get-AzureVM
+	if ($VMLastStatus -eq $VM.Status) { 
+		Write-Host "." -NoNewline
+	} else {
+		Write-Host "`n$($VM.Status)" -NoNewline
+	}
+	$VMLastStatus = $VM.Status
+	Start-Sleep -Seconds 1
+}
+
+if ($VM.Status.Equals("ReadyRole")) {
+	Write-Host "VM deployed succesfully"
+} else {
+	Write-Host "VM deployed with errors"
+}
