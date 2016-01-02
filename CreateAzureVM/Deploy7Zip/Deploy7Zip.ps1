231a5e42b5b926594e2afa2cf77590d3846ed2a7
jonhcw <jonhcw@gmail.com>
2016-01-02 13:30:41 +0200

    Created project for 7-zip deployment and added script with basic variables

---
+++ CreateAzureVM/Deploy7Zip/Deploy7Zip.ps1
@@ -0,0 +1,10 @@
+﻿#Requires -Modules Azure
+
+# URL to installer executable
+$Media = "http://www.7-zip.org/a/7z1514-x64.exe"
+$LocalFile = "7-Zip-installer.exe"
+$DownloadFolder = "C:\Media\"
+$InstallParameters = ' /S /D="C:\Program Files\7-Zip"'
+$InstallCommand = $DownloadFolder + $LocalFile + $InstallParameters
+Write-Host $InstallCommand
+
